f096cd1985c9c5d9c30b8f7915b45ab000802bbb
Jose Manuel Cuello Royo <56176862+Josemcr1134@users.noreply.github.com>
2024-03-16 20:48:21 -0500

    Update environment.prod.ts

--- Botox_affiliations-main/src/environments/environment.prod.ts
+++ Botox_affiliations-main/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    base_url: 'https://pygfurughz.hidoctor.app',
+    base_url: 'https://pygfurughz.hidoctor.app/api',
     wompi_url: 'https://sandbox.wompi.co/v1',
     pub_wompi_test: 'pub_test_0lFRLbzcrYKgi6AyL3qLBThez7I6KRki',
     wompi_redirec_url: 'https://pre-affiliations.netcare.life/payment-response',
